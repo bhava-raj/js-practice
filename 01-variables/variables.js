4e848560e883ea9e5a19b0b1bbd461fee7ab9a0d
Bhava-raj <bhava.raj.200515@gmail.com>
2025-08-21 14:55:01 +0530

    Version 1 - Basics of Variables

---
+++ 01-variables/variables.js
@@ -0,0 +1,25 @@
+// Variables Basics in JavaScript
+
+// Example 1: Store your name
+let name = "Ace";
+console.log("My name is " + name);
+
+// Example 2: Store age
+let age = 20;
+console.log("I am " + age + " years old");
+
+// Example 3: Reassign variable
+let city = "Chennai";
+console.log("I live in " + city);
+city = "Bangalore";
+console.log("Now I moved to " + city);
+
+// Example 4: Use const (cannot be changed)
+const country = "India";
+console.log("I am from " + country);
+
+// Example 5: Simple math
+let a = 10;
+let b = 5;
+let sum = a + b;
+console.log("The sum is: " + sum);
