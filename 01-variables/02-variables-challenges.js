ee53ec42e6f17e1d79d9d6f710414f7ddda0e6e1
Bhava-raj <bhava.raj.200515@gmail.com>
2025-08-21 23:53:40 +0530

    Version 2 - Variable Challenges

---
+++ 01-variables/02-variables-challenges.js
@@ -0,0 +1,41 @@
+//1st 
+let a = 5;
+let b = 10;
+
+console.log("Before swap: a =", a, ", b =", b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("After swap: a =", a, ", b =", b);
+
+//2nd
+let studentName = "Ace";
+let studentAge = 20;
+let grade = "A";
+
+console.log(studentName + " is " + studentAge + " years old and got grade " + grade);
+
+//3rd
+let length = 8;
+let width = 6;
+let area = length * width;
+
+console.log("The area of rectangle is: " + area);
+
+//4th
+let score = 0;
+score = score + 10;
+score = score + 10;
+score = score - 5;
+
+console.log("Final Score: " + score);
+
+//5th
+const pi = 3.14159;
+let radius = 7;
+let circumference = 2 * pi * radius;
+
+console.log("Circumference of circle: " + circumference);
+
