81bd7ceafdf853081032a0ae1050eb9a6145c08a
Bhava-raj <bhava.raj.200515@gmail.com>
2025-08-22 14:49:19 +0530

    Version 2- Operators Practice

---
+++ 03-Operators/operators.js
@@ -0,0 +1,28 @@
+let num = 8
+
+if (num % 2 === 0) {
+  console.log(num + " is even");
+} else {
+  console.log(num + " is odd");
+}
+
+let 
+a=3
+b=6
+
+if (a > b) {
+  console.log(a + " is greater than " + b);
+}else{
+  console.log(b + " is greater than " + a);
+}
+
+let x=true
+y=false
+
+console.log(!x);
+console.log(x && y);
+
+let fun=false
+console.log(`Before flipping fun: ${fun}`);
+fun=!fun
+console.log(`After flipping fun: ${fun}`); 
